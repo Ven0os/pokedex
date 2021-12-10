41b76895a473df08940e18ea652cbea7708ad4f3
Ven0os <fredouelletheo@gmail.com>
2021-12-10 20:39:55 +0100

    update

--- pokedex.js
+++ pokedex.js
@@ -1,59 +1,62 @@
 const pokedex = document.getElementById("pokedex");
 
-const fetchPokemon = () => {
+const fetchPokemons = () => {
   const promises = [];
-  for (let i = 1; i <= 15; i++) {
+  for (let i = 1; i <= 896; i++) {
     const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
-    promises.push(fetch(url).then((res) => res.json()));
+    promises.push(
+      fetch(url)
+        .then((res) => res.json())
+        .then((pokemon) => ({
+          ...pokemon,
+          images: {
+            sprite: pokemon.sprites["front_default"],
+            shiny: pokemon.sprites["front_shiny"],
+          },
+          type: pokemon.types.map((type) => type.type.name).join(", "),
+          // gen: pokemon.game_indices.map((type) => type.type.name).join(", "),
+        }))
+    );
   }
-  Promise.all(promises).then((results) => {
-    const pokemon = results.map((result) => ({
-      name: result.name,
-      image: result.sprites["front_default"],
-      images: result.sprites["front_shiny"],
-      type: result.types.map((type) => type.type.name).join(", "),
-      id: result.id,
-      height: result.height,
-      weight: result.weight,
-    }));
-
-    displayPokemon(pokemon);
-    console.log(results);
-  });
+  return Promise.all(promises);
 };
 
-// Ajouts des cartes au code Html
+function displayAllPokemons(pokemons) {
+  for (const pokemon of pokemons) {
+    displayPokemon(pokemon);
+  }
 
-const displayPokemon = (pokemon) => {
-  console.log(pokemon);
-  const pokemonHTMLString = pokemon
-    .map(
-      (pokemon) => `
-        <li class="cartepkm" data-pokeid="${pokemon.id}">
-        <div class="cartebgr">
-        <img class="cartepkm-image" src="${pokemon.image}"/>
-            <img class="cartepkm-shiny" data-pokeid="${pokemon.id}" src="${pokemon.images}"/>
-            <h2 class="cartepkm-title">${pokemon.id} - ${pokemon.name}</h2>
-            <p class="cartepkm-subtitle">Type: ${pokemon.type}</p>
-            <p class="cartepkm-subtitle">Poids et taille: ${pokemon.height}m ${pokemon.weight}kg</p>
-        </div>
-        </li>
-    `
-    )
-    .join("");
-
-  pokedex.innerHTML = pokemonHTMLString;
-  let afficherShiny = document.querySelectorAll(".cartepkm");
-
-  afficherShiny.forEach((element) =>
+  document.querySelectorAll(".cartepkm").forEach((element) =>
     element.addEventListener("click", (event) => {
-      const id = event.target.getAttribute("data-pokeid");
-      const cacherShiny = document.querySelector(
-        `.cartepkm-shiny[data-pokeid="${id}"]`
-      );
-      cacherShiny.style.display = "block";
+      const id = getPokeId(event.target);
+      document
+        .querySelector(`.cartepkm-shiny[data-pokeid="${id}"]`)
+        .classList.toggle("cartepkm-image");
     })
   );
-};
+}
 
-fetchPokemon();
+function getPokeId(element) {
+  let id = element.getAttribute("data-pokeid");
+  if (!id) id = getPokeId(element.parentElement);
+  return id;
+}
+
+function displayPokemon(pokemon) {
+  const html = `<li class="cartepkm" data-pokeid="${pokemon.id}">
+  <div class="cartebgr">
+  <img class="cartepkm-image" src="${pokemon.images.sprite}"/>
+  <img class="cartepkm-shiny" data-pokeid="${pokemon.id}" src="${pokemon.images.shiny}"/>
+  <h2 class="cartepkm-title">${pokemon.id} - ${pokemon.name}</h2>
+  <p class="cartepkm-subtitle">Type: ${pokemon.type}</p>
+  <p class="cartepkm-subtitle">Poids et taille: ${pokemon.height}m ${pokemon.weight}kg</p>
+  </div>
+  </li>`;
+  pokedex.innerHTML += html;
+}
+
+document.addEventListener(`DOMContentLoaded`, () => {
+  fetchPokemons().then((pokemons) => {
+    displayAllPokemons(pokemons);
+  });
+});
